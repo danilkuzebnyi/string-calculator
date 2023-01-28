3ff6436f61bed4237cd9566f1e04c6cf075345d1
Danil Kuzebnyi <danylo.k@air-dr.com>
2023-01-29 00:43:22 +0200

    write code

--- src/main/java/org/danylo/InputTools.java
+++ src/main/java/org/danylo/InputTools.java
@@ -1,9 +1,14 @@
 package org.danylo;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputTools {
     public static List<Integer> parseString(String line) {
-        return null;
+        return Arrays.stream(line.split("[^\\d|\\-]+"))
+                .filter(symbol -> !symbol.equals(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
